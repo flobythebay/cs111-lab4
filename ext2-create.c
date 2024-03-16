28e53493bc38d1ee176572b7d04b3d194d8f5a1a
Florence <florence.y.zhao@gmail.com>
2024-03-15 19:35:06 -0700

    use const name instead of number

--- ext2-create.c
+++ ext2-create.c
@@ -208,7 +208,7 @@ void write_superblock(int fd) {
 	superblock.s_log_frag_size = 0;						/* 1024 */
 	superblock.s_blocks_per_group = BLOCK_SIZE * 8;
 	superblock.s_frags_per_group = BLOCK_SIZE * 8;
-	superblock.s_inodes_per_group = 128;
+	superblock.s_inodes_per_group = NUM_INODES;
 	superblock.s_mtime = 0;				/* Mount time */
 	superblock.s_wtime = current_time;	/* Write time */
 	superblock.s_mnt_count         = 0; /* Number of times mounted so far */
